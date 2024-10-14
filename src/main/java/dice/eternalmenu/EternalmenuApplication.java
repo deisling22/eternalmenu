23c943e6e4a1633894915770027c14ed4c866eda
Roman <roman_deisling@live.de>
2024-10-14 16:59:50 +0200

    first commit

---
+++ src/main/java/dice/eternalmenu/EternalmenuApplication.java
@@ -0,0 +1,13 @@
+package dice.eternalmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternalmenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EternalmenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dice/eternalmenu/EternalmenuApplicationTests.java
@@ -0,0 +1,13 @@
+package dice.eternalmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EternalmenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
